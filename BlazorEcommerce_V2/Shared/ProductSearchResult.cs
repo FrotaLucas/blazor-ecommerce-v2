3b7d06a96093836e36098e7a0cbfdbb2dffd6831
FrotaLucas <lucasdiasfrota@gmail.com>
2024-04-17 17:25:36 +0200

    create ProductSearch DTO

---
+++ BlazorEcommerce_V2/Shared/ProductSearchResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorEcommerce_V2.Shared
+{
+    internal class ProductSearchResult
+    {
+    }
+}
