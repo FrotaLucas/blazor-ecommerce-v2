a995748bb83a7dc88b0c1984124f2d77f24a01ab
FrotaLucas <lucasdiasfrota@gmail.com>
2024-04-23 18:05:48 +0200

    create post method in CartController

---
+++ BlazorEcommerce_V2/Server/Controllers/CartController.cs
@@ -0,0 +1,26 @@
+﻿using BlazorEcommerce_V2.Server.Services.CartService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorEcommerce_V2.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        public readonly ICartService _cartService;
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        [HttpPost("products")]
+        public async Task<ActionResult<ServiceResponse<List<CartProductResponse>>>> GetCartProducts(List<CartItem> cartItems)
+        {
+            var result = await _cartService.GetCartProducts(cartItems);
+
+            return Ok(result);
+        }
+    }
+}
+    
