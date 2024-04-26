0558ed01ecc5fd2a3750ea0c76db88a4092d88f4
FrotaLucas <lucasdiasfrota@gmail.com>
2024-04-26 19:22:16 +0200

    delete ProductDetailss component from folder shared

---
+++ BlazorEcommerce_V2/Shared/UserRegister.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorEcommerce_V2.Shared
+{
+
+    public class UserRegister
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
