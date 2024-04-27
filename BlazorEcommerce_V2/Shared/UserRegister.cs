6a11546e0063c00a4854a06bbc81015921182874
FrotaLucas <lucasdiasfrota@gmail.com>
2024-04-27 12:17:28 +0200

    create new componenet called UserButton

--- BlazorEcommerce_V2/Shared/UserRegister.cs
+++ BlazorEcommerce_V2/Shared/UserRegister.cs
@@ -10,8 +10,11 @@ namespace BlazorEcommerce_V2.Shared
 
     public class UserRegister
     {
+        [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
+        [Required, StringLength(100, MinimumLength = 6)]
         public string Password { get; set; } = string.Empty;
+        [Compare ("Password", ErrorMessage = "The password do not match.")]
         public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
