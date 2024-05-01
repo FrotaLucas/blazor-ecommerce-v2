240015913532a5340b2ddba8202d0b379a52d103
FrotaLucas <lucasdiasfrota@gmail.com>
2024-05-01 19:14:39 +0200

    create class CusomAuthStateProvider

---
+++ BlazorEcommerce_V2/Client/CustomAuthStateProvider.cs
@@ -0,0 +1,76 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace BlazorEcommerce_V2.Client
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorageService;
+        private readonly HttpClient _httpClient;
+        public CustomAuthStateProvider(ILocalStorageService LocalStorageService, HttpClient http)
+        {
+            _httpClient = http;
+            _localStorageService = LocalStorageService;
+        }
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var authToken = await _localStorageService.GetItemAsStringAsync("authToken");
+            
+            var identity = new ClaimsIdentity();
+            //checar se essa secao Authorization esta sendo criada dentro da HTTP request !!!!!!!!!!!!!!
+            _httpClient.DefaultRequestHeaders.Authorization = null;
+
+            if(string.IsNullOrEmpty(authToken))
+            {
+                try
+                {
+                    identity = new ClaimsIdentity(ParseFromClaimsJwt(authToken), "jwt");
+                    _httpClient.DefaultRequestHeaders.Authorization =
+                        new AuthenticationHeaderValue("Bearer", authToken.Replace("\"", ""));
+                }
+                catch
+                {
+                    _localStorageService.RemoveItemAsync("authToken");
+                    identity = new ClaimsIdentity();
+                }
+
+
+            }
+
+            var user = new ClaimsPrincipal(identity);
+            var state = new AuthenticationState(user);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+
+            return state;
+        }
+
+        private byte[] ParseBase64WithoutPadding(string base64)
+        {
+            switch (base64.Length % 4)
+            {
+                case 2: base64 += "=="; break;
+                case 3: base64 += "="; break;
+               
+            }
+
+            //converte em array de ASCII
+            return Convert.FromBase64String(base64);
+        }
+
+        private IEnumerable<Claim> ParseFromClaimsJwt(string jwt)
+        {
+            //pegando somente o payload do Token gerado para transformar em um IEnumerable de Claim
+            var payload = jwt.Split('.')[1];
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+
+            var claims = keyValuePairs.Select(kv => new Claim(kv.Key, kv.Value.ToString()));
+
+            return claims;
+        }
+    }
+}
