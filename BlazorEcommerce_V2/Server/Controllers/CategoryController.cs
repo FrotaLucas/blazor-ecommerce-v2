06f3d0032a948761d43d6aa7f819029b268c7f6b
FrotaLucas <lucasdiasfrota@gmail.com>
2024-08-18 20:02:26 +0200

    create apis to Category Service for Admin

--- BlazorEcommerce_V2/Server/Controllers/CategoryController.cs
+++ BlazorEcommerce_V2/Server/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BlazorEcommerce_V2.Server.Services.CategoryService;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,12 +16,43 @@ namespace BlazorEcommerce_V2.Server.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<ServiceResponse<List<Category>>>> Get()
+        public async Task<ActionResult<ServiceResponse<List<Category>>>> GetCategories()
         {
             //preciso do wait aqui novamente mesmo sabendo que dentro de _categoryService esta sendo usado await?????
             var category = await _categoryService.GetCategories();
             return Ok(category);
+        } 
+        
+        [HttpGet("admin"), Authorize(Roles = "Admin")]
+        public async Task<ActionResult<ServiceResponse<List<Category>>>> GetAdminCategories()
+        {
+            var result = await _categoryService.GetAdminCategories();
+            return Ok(result);
         }
+
+        [HttpDelete("admin"), Authorize(Roles = "Admin")]
+        public async Task<ActionResult<ServiceResponse<List<Category>>>> DeleteCategory(int id)
+        {
+            var result = await _categoryService.DeleteCategory(id);
+            return Ok(result);
+        }
+        
+        [HttpPost("admin"), Authorize(Roles = "Admin")]
+        public async Task<ActionResult<ServiceResponse<List<Category>>>> AddCategories(Category category)
+        {
+            var result = await _categoryService.AddCategories(category);
+            return Ok(result);
+        }
+        
+        [HttpPut("admin"), Authorize(Roles = "Admin")]
+        public async Task<ActionResult<ServiceResponse<List<Category>>>> UpdateCategory(Category category)
+        {
+            var result = await _categoryService.UpdateCategory(category);
+            return Ok(result);
+        }
+
+
+
     }
 
         
